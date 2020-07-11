576a9283479c40e4930b7d0fe5a28c56deba8867
cool-7up <cool7up@naver.com>
2020-07-12 03:04:48 +0900

    Updates

---
+++ precache-manifest.2230869d65305b91a2904003376a167a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b5e7894f93f1e17792b5a345b56be8",
+    "url": "/moonflix/index.html"
+  },
+  {
+    "revision": "1e85d0506f9630f73661",
+    "url": "/moonflix/static/css/main.b1d8055b.chunk.css"
+  },
+  {
+    "revision": "507aec3484a85988befa",
+    "url": "/moonflix/static/js/2.6e248bee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/moonflix/static/js/2.6e248bee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e85d0506f9630f73661",
+    "url": "/moonflix/static/js/main.a2f98f48.chunk.js"
+  },
+  {
+    "revision": "3126f5cfbf3e77837718",
+    "url": "/moonflix/static/js/runtime-main.268efc3c.js"
+  }
+]);
